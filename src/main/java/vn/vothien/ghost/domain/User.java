4e4ded93519f69a296d93ea45eff36bded0ff32e
ghostoflord <ghost2892003zzz@gmail.com>
2025-02-06 16:38:54 +0700

    create project

---
+++ src/main/java/vn/vothien/ghost/domain/User.java
@@ -0,0 +1,22 @@
+package vn.vothien.ghost.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+// @Entity
+// @Table(name = "User")
+public class User {
+    // @Id
+    // @GeneratedValue(strategy = GenerationType.IDENTITY)
+    // @Column(name = "id", length = 30)
+    // private long id;
+    // @Column(name = "User_name", length = 30)
+    // private int userName;
+    // @Column(name = "email", length = 30)
+    // private int email;
+
+}

9dd2362b4a5837b6d76b677c5e9c7a2f50cd2f92
ghostoflord <ghost2892003zzz@gmail.com>
2025-02-07 11:10:28 +0700

    fix crud user

--- src/main/java/vn/vothien/ghost/controller/UserCotroller.java
+++ src/main/java/vn/vothien/ghost/controller/UserCotroller.java
@@ -1,5 +1,9 @@
 package vn.vothien.ghost.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+ 
 public class UserCotroller {
 
 }
--- src/main/java/vn/vothien/ghost/service/UserService.java
+++ src/main/java/vn/vothien/ghost/service/UserService.java
@@ -1,8 +1,75 @@
 package vn.vothien.ghost.service;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
+
+import vn.vothien.ghost.domain.User;
+import vn.vothien.ghost.repository.UserRepository;
 
 @Service
 public class UserService {
+    private final UserRepository userRepository;
 
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    // handle create user
+    public User handleCreateUser(User user) {
+        // // check company
+        // if (user.getCompany() != null) {
+        // Optional<Company> companyOptional =
+        // this.companyService.findById(user.getCompany().getId());
+        // user.setCompany(companyOptional.isPresent() ? companyOptional.get() : null);
+        // }
+
+        // // check role
+        // if (user.getRole() != null) {
+        // Role r = this.roleService.fetchRoleById(user.getRole().getId());
+        // user.setRole(r != null ? r : null);
+        // }
+        return this.userRepository.save(user);
+    }
+
+    // take user by id
+    public User fetchUserById(long id) {
+        Optional<User> userOptional = this.userRepository.findById(id);
+        if (userOptional.isPresent()) {
+            return userOptional.get();
+        }
+        return null;
+    }
+
+    // update user
+    public User handleUpdateUser(User reqUser) {
+        User currentUser = this.fetchUserById(reqUser.getId());
+        if (currentUser != null) {
+            currentUser.setAddress(reqUser.getAddress());
+            currentUser.setGender(reqUser.getGender());
+
+            // check company
+            // if (reqUser.getCompany() != null) {
+            // Optional<Company> companyOptional =
+            // this.companyService.findById(reqUser.getCompany().getId());
+            // currentUser.setCompany(companyOptional.isPresent() ? companyOptional.get() :
+            // null);
+            // }
+
+            // check role
+            // if (reqUser.getRole() != null) {
+            // Role r = this.roleService.fetchRoleById(reqUser.getRole().getId());
+            // currentUser.setRole(r != null ? r : null);
+            // }
+
+            // update
+            currentUser = this.userRepository.save(currentUser);
+        }
+        return currentUser;
+    }
+
+    // delete use
+    public void handleDeleteUser(long id) {
+        this.userRepository.deleteById(id);
+    }
 }

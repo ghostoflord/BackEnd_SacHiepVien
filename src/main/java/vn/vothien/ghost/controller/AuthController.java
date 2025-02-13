25445b4861a25d68a6b9125eaf7d987f09570093
ghostoflord <ghost2892003zzz@gmail.com>
2025-02-13 16:18:30 +0700

    update login and add jwt

--- src/main/java/vn/vothien/ghost/controller/AuthController.java
+++ src/main/java/vn/vothien/ghost/controller/AuthController.java
@@ -1,34 +1,40 @@
 package vn.vothien.ghost.controller;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import vn.vothien.ghost.domain.dto.LoginDTO;
+import vn.vothien.ghost.domain.response.ResLoginDTO;
+import vn.vothien.ghost.util.SecurityUtil;
 
 @RestController
 public class AuthController {
 
     private final AuthenticationManagerBuilder authenticationManagerBuilder;
+    private final SecurityUtil securityUtil;
 
-    public AuthController(AuthenticationManagerBuilder authenticationManagerBuilder) {
+    public AuthController(AuthenticationManagerBuilder authenticationManagerBuilder, SecurityUtil securityUtil) {
         this.authenticationManagerBuilder = authenticationManagerBuilder;
+        this.securityUtil = securityUtil;
     }
 
     @PostMapping("/login")
-    public ResponseEntity<LoginDTO> authenticateUser(@RequestBody LoginDTO loginDto) {
+    public ResponseEntity<ResLoginDTO> authenticateUser(@RequestBody LoginDTO loginDto) {
         // Nạp input gồm username/password vào Security
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
                 loginDto.getUsername(), loginDto.getPassword());
         // xác thực người dùng => cần viết hàm loadUserByUsername
         Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
-        return ResponseEntity.ok().body(loginDto);
+        // create a token
+        String access_token = this.securityUtil.createToken(authentication);
+        ResLoginDTO res = new ResLoginDTO();
+        res.setAccessToken(access_token);
+        return ResponseEntity.ok().body(res);
     }
 
 }
---
+++ src/main/java/vn/vothien/ghost/domain/response/ResLoginDTO.java
@@ -0,0 +1,13 @@
+package vn.vothien.ghost.domain.response;
+
+public class ResLoginDTO {
+    private String accessToken;
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+}

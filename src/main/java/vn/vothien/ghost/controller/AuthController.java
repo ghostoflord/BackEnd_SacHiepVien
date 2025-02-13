289192b15ab5f332ae0f59bc48b3a9b62e6dd7cf
ghostoflord <ghost2892003zzz@gmail.com>
2025-02-13 19:49:18 +0700

    fix

--- src/main/java/vn/vothien/ghost/controller/AuthController.java
+++ src/main/java/vn/vothien/ghost/controller/AuthController.java
@@ -8,8 +8,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import vn.vothien.ghost.domain.User;
 import vn.vothien.ghost.domain.dto.LoginDTO;
 import vn.vothien.ghost.domain.response.ResLoginDTO;
+import vn.vothien.ghost.service.UserService;
 import vn.vothien.ghost.util.SecurityUtil;
 
 @RestController
@@ -17,10 +19,13 @@ public class AuthController {
 
     private final AuthenticationManagerBuilder authenticationManagerBuilder;
     private final SecurityUtil securityUtil;
+    private final UserService userService;
 
-    public AuthController(AuthenticationManagerBuilder authenticationManagerBuilder, SecurityUtil securityUtil) {
+    public AuthController(AuthenticationManagerBuilder authenticationManagerBuilder, SecurityUtil securityUtil,
+            UserService userService) {
         this.authenticationManagerBuilder = authenticationManagerBuilder;
         this.securityUtil = securityUtil;
+        this.userService = userService;
     }
 
     @PostMapping("/login")
@@ -33,6 +38,15 @@ public class AuthController {
         // create a token
         String access_token = this.securityUtil.createToken(authentication);
         ResLoginDTO res = new ResLoginDTO();
+
+        User currentUserDB = this.userService.handleGetUserByUsername(loginDto.getUsername());
+        if (currentUserDB != null) {
+            ResLoginDTO.UserLogin userLogin = new ResLoginDTO.UserLogin(
+                    currentUserDB.getId(),
+                    currentUserDB.getEmail(),
+                    currentUserDB.getUserName());
+            res.setUser(userLogin);
+        }
         res.setAccessToken(access_token);
         return ResponseEntity.ok().body(res);
     }
